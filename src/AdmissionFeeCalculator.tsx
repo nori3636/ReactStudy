84172e2c406c6bae5ef98b5f66e635f93e73923a
nori3636 <nori.k3636@gmail.com>
2020-11-27 16:18:39 +0900

    リストを作成

--- src/AdmissionFeeCalculator.tsx
+++ src/AdmissionFeeCalculator.tsx
@@ -5,40 +5,41 @@ import {FeeClassification,AdmissionFeeCalculatorState} from './types';
 
 // クラスの配列を作る
 const AdmissionFeeCalculator: React.FC<AdmissionFeeCalculatorState>=props=>{
-    const [adults, students, children, infants]  = useState(0);
-        const adults: FeeClassification = {
+    // const [adults, students, children, infants]  = useState(0);
+    const fee: FeeClassification[] = [
+        {
             name: "大人",
             description: "",
             unitPrice: 1000,
             numOfPeople: 0,
             totalPrice: 0,
-        };
-        const students: FeeClassification = {
+        },
+        {
             name: "学生",
             description: "中学生",
             unitPrice: 700,
             numOfPeople: 0,
             totalPrice: 0,
-        };
-        const children: FeeClassification = {
+        },
+        {
             name: "子ども",
             description: "小学生",
             unitPrice: 300,
             numOfPeople: 0,
             totalPrice: 0,
-        };
-        const infants: FeeClassification = {
+        },
+        {
             name: "幼児",
             description: "未就学",
             unitPrice: 0,
             numOfPeople: 0,
             totalPrice: 0,
-        };
-        this.state = { feeClassifications: [adults, students, children, infants] };
-    }
+        }
+    ];
+    //     this.state = { feeClassifications: [adults, students, children, infants] };
 
     // chengeイベントの処理
-    chandleNumOfPeopleChange(idx: number, num: number) {
+    handleNumOfPeopleChange(idx: number, num: number) {
         const currentFC = state.feeClassifications[idx];
         const newTotalPrice = currentFC.unitPrice * num;
         // 今あるものをコピペ。ただしトータルは変更
@@ -52,8 +53,8 @@ const AdmissionFeeCalculator: React.FC<AdmissionFeeCalculatorState>=props=>{
                 <Detail key={idx.toString()} classification={fc} onNumOfPeopleChange={n => this.handleNumOfPeopleChange(idx, n)} />
             );
         });
-        const numOfPeople = this.state.feeClassifications.map(fc => fc.numOfPeople).reduce((p, c) => p + c);
-        const totalAmount = this.state.feeClassifications.map(fc => fc.totalPrice).reduce((p, c) => p + c);
+        const numOfPeople = props.feeClassifications.map(fc => fc.numOfPeople).reduce((p, c) => p + c);
+        const totalAmount = props.feeClassifications.map(fc => fc.totalPrice).reduce((p, c) => p + c);
 
         return (
             <>
