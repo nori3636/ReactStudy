e7d919ec272ffa29fffb8eb99c4a59d28bd22257
nori3636 <nori.k3636@gmail.com>
2020-11-27 15:56:24 +0900

    外径をそれっぽく

--- src/AdmissionFeeCalculator.tsx
+++ src/AdmissionFeeCalculator.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Summary from './Summary';
 import Detail from './Detail';
 import {FeeClassification,AdmissionFeeCalculatorState} from './types';
 
 // クラスの配列を作る
-class AdmissionFeeCalculator extends React.Component<{},AdmissionFeeCalculatorState>{
-    constructor(props: {}) {
-        super(props);
+const AdmissionFeeCalculator: React.FC<AdmissionFeeCalculatorState>=props=>{
+    const [adults, students, children, infants]  = useState(0);
         const adults: FeeClassification = {
             name: "大人",
             description: "",
@@ -39,17 +38,15 @@ class AdmissionFeeCalculator extends React.Component<{},AdmissionFeeCalculatorSt
     }
 
     // chengeイベントの処理
-    handleNumOfPeopleChange(idx: number, num: number) {
-        const currentFC = this.state.feeClassifications[idx];
+    chandleNumOfPeopleChange(idx: number, num: number) {
+        const currentFC = state.feeClassifications[idx];
         const newTotalPrice = currentFC.unitPrice * num;
         // 今あるものをコピペ。ただしトータルは変更
         const newFC: FeeClassification =　Object.assign({}, currentFC, { numOfPeople: num, totalPrice: newTotalPrice });
-        const feeClassifications = this.state.feeClassifications.slice();　//型宣言しなおしてる？
+        const feeClassifications = state.feeClassifications.slice();　//型宣言しなおしてる？
         feeClassifications[idx] = newFC;
         this.setState({ feeClassifications: feeClassifications });
     }
-
-    render() {
         const details = this.state.feeClassifications.map((fc, idx) => {
             return (
                 <Detail key={idx.toString()} classification={fc} onNumOfPeopleChange={n => this.handleNumOfPeopleChange(idx, n)} />
